d49b5bac788f5b37a10e36e234114c1eb213567d
Jill Tankersley <jillian.emilie@gmail.com>
2016-05-05 23:36:25 -0400

    setup views

--- jedi_awakens/ViewController.swift
+++ jedi_awakens/ViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var forceViewer: UILabel!
+    
+    enum JSONError: String, ErrorType {
+        case NoData = "ERROR: no data"
+        case ConversionFailed = "ERROR: conversion from JSON failed"
+    }
+    
+    func jsonParser() {
+        let urlPath = "http://ajediempireawakens.herokuapp.com/api/v1/generator.json"
+        guard let endpoint = NSURL(string: urlPath) else {
+            print("Error creating endpoint")
+            return
+        }
+        let request = NSMutableURLRequest(URL:endpoint)
+        NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) in
+            do {
+                guard let data = data else {
+                    throw JSONError.NoData
+                }
+                guard let json = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary else {
+                    throw JSONError.ConversionFailed
+                }
+                print(json)
+            } catch let error as JSONError {
+                print(error.rawValue)
+            } catch let error as NSError {
+                print(error.debugDescription)
+            }
+            }.resume()
+    }
+    
+    @IBAction func getAnswerTapped(sender: AnyObject) {
+        
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
